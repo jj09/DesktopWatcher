4c5fa848716a62a66e3d9596cce05de22d18ac1c
jj09 <jakub.jedryszek@gmail.com>
2013-06-09 16:23:21 -0500

    Version alpha

---
+++ DesktopWatcher/DesktopWatcher/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace DesktopWatcher
+{
+    public partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.play = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(10, 10);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // play
+            // 
+            this.play.Location = new System.Drawing.Point(29, 6);
+            this.play.Name = "play";
+            this.play.Size = new System.Drawing.Size(75, 23);
+            this.play.TabIndex = 1;
+            this.play.Text = "Play";
+            this.play.UseVisualStyleBackColor = true;
+            this.play.Click += new System.EventHandler(this.play_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(116, 41);
+            this.Controls.Add(this.play);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button play;
+    }
+}
+
---
+++ DesktopWatcher/DesktopWatcher/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DesktopWatcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private MouseMoveMessageFilter mouseMessageFilter;
+        public Point mousePosition;
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+
+            Application.RemoveMessageFilter(this.mouseMessageFilter);
+        }
+
+        public void CloseForm()
+        {
+            this.Close();
+        }
+
+        public class MouseMoveMessageFilter : IMessageFilter
+        {
+            public dynamic TargetForm { get; set; }
+
+            public bool PreFilterMessage(ref Message m)
+            {
+                int numMsg = m.Msg;
+                if (numMsg == 0x0200 /*WM_MOUSEMOVE*/ || numMsg == 0x0100 /*WM_KEYDOWN*/ || numMsg == 0x0104  /*WM_SYSKEYDOWN*/)
+                {
+                    this.TargetForm.Text = string.Format("X:{0}, Y:{1}", Control.MousePosition.X, Control.MousePosition.Y);
+                    
+                    if (this.TargetForm.mousePosition.X != Cursor.Position.X || numMsg == 0x0100 /*WM_KEYDOWN*/ || numMsg == 0x0104  /*WM_SYSKEYDOWN*/)
+                    {
+                        this.TargetForm.axWindowsMediaPlayer1.Ctlcontrols.play();
+                        LockWorkStation();
+                    }
+                }
+
+                return false;
+            }
+
+            [DllImport("user32.dll")]
+            public static extern void LockWorkStation();
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+        }        
+
+        private void play_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                axWindowsMediaPlayer1.URL = openFileDialog1.FileName;
+            }
+            System.Threading.Thread.Sleep(5000);    // 5 seconds delay before mouse moving stop 
+            mousePosition.X = Cursor.Position.X;
+            mousePosition.Y = Control.MousePosition.Y;
+
+            this.FormClosing -= new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+
+            this.mouseMessageFilter = new MouseMoveMessageFilter();
+            this.mouseMessageFilter.TargetForm = this;
+            Application.AddMessageFilter(this.mouseMessageFilter);
+        }
+    }
+}
